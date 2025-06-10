9a1a06a8abc4ffc75ac8740b112f35d8a3e4796c
elvisjsAtlas1 <chat02jselvis@gmai.com>
2025-06-10 18:27:20 -0500

    quitando matricula

--- acad-matricula-service/src/main/java/com/acad/matriculaservice/Repository/MatriculaRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.acad.matriculaservice.Repository;
-
-import com.acad.matriculaservice.Entity.Matricula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface MatriculaRepositorio extends JpaRepository<Matricula, Long> {
-}

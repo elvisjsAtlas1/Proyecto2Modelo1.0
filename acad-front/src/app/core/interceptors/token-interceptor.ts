9c3fc5f5e3efe8ff9dd989909bc595730b581d2d
elvisjsAtlas1 <chat02jselvis@gmail.com>
2025-06-19 09:23:38 -0500

    acad-front modelo terminado

--- acad-front/src/app/core/interceptors/token-interceptor.ts
+++ acad-front/src/app/core/interceptors/token-interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   //const token = localStorage.getItem('token');
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJub2UudGlwbyIsImlkIjoxLCJpYXQiOjE3NTAyNTQ2NDgsImV4cCI6MTc1MDI1ODI0OH0.w6QMOaAyj5w6ZI8czkNy6wVtYuAzJ3-hiGjDP8FYny8";
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbHZpcyIsImlkIjoxLCJpYXQiOjE3NTAzNDA3MDQsImV4cCI6MTc1MDM0NDMwNH0.d2WjFl6QCFPGnuUSPOIIkvhNwVMnQrcxhUOkpyjmHaA";
 
   if (token) {
     const authReq = req.clone({
